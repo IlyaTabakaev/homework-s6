d5cf0c1465b71da8a8c2b8697dfad554f119f5c7
Ilya <osetia9595@gmail.com>
2023-01-13 14:56:39 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("input count numbers");
+int countNumbers = int.Parse(Console.ReadLine());
+int[] array = new int[countNumbers];
+int count = 0;
+
+for (int i = 0; i <array.Length; i++)
+{
+    Console.WriteLine("input your number");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+
+for (int i = 0; i <array.Length; i++)
+{
+Console.Write(array[i] + ",");
+}
+ 
+for (int i = 0; i <array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"count of positive numbers = {count}");
