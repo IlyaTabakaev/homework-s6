a5add83412f537a360da5d56ea1c11986c50df8a
Ilya <osetia9595@gmail.com>
2023-01-13 15:37:48 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+Console.WriteLine("input b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+
+ 
+Console.WriteLine($"lines cross in: ({x};{y})");
